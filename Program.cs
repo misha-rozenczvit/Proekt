13533f6f7d363fcda9d369c77aa5732621318772
PC <PC@DESKTOP-EQOK7T5>
2023-12-23 19:27:14 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _124
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write(" Введите Год: ");
+            //double year = double.Parse (Console.ReadLine());
+            //switch ( year)
+            //{
+            //    case 2020: Console.WriteLine("Год Крысы "); break;
+            //    case 2008: Console.WriteLine("Год Крысы"); break;
+            //    case 1996: Console.WriteLine("Год Крысы"); break;
+            //    case 1984: Console.WriteLine("Год Крысы"); break;
+                   
+            //    case 1972: Console.WriteLine("Год Крысы"); break;
+            //    case 1960: Console.WriteLine("Год Крысы"); break;
+            //    case 1936:Console.WriteLine("Год Крысы"); break;
+            //        Console.WriteLine("Год Крысы"); break;
+            //    case 2021 :Console.WriteLine("Год Быка "); break;
+            //    case 2009: Console.WriteLine("Год Быка"); break;
+            //    case 1997: Console.WriteLine("Год Быка"); break;
+            //    case 1985: Console.WriteLine("Год Быка"); break;
+
+            //    case 1973: Console.WriteLine("Год Быка"); break;
+            //    case 1961: Console.WriteLine("Год Быка"); break;
+            //    case 1949:Console.WriteLine("Год Быка"); break;
+            //    case 1937: Console.WriteLine("Год Быка"); break;
+            //        Console.WriteLine("Год Быка"); break;
+            //    case 2022: Console.WriteLine(" Год Тигра "); break;
+            //    case 2010: Console.WriteLine("Год Тигра"); break;
+            //    case 1998: Console.WriteLine("Год Тигра"); break;
+            //    case 1986: Console.WriteLine("Год Тигра"); break;
+
+            //    case 1974: Console.WriteLine("Год Тигра"); break;
+            //    case 1962: Console.WriteLine("Год Тигра"); break;
+            //    case 1950: Console.WriteLine("Год Тигра"); break;
+            //    case 1938: Console.WriteLine("Год Тигра"); break;
+            //        Console.WriteLine("Год Тигра"); break;
+            //    case 2023: Console.WriteLine("Год Кролика"); break;
+            //    case 2011: Console.WriteLine("Год Кролика");  break ;
+            //    case 1999: Console.WriteLine("Год Кролика"); break;
+            //    case 1987: Console.WriteLine("Год Кролика"); break;
+
+            //    case 1975: Console.WriteLine("Год Кролика"); break;
+            //    case 1963: Console.WriteLine("Год Кролика"); break;
+            //    case 1951: Console.WriteLine("Год Кролика"); break;
+            //    case 1939: Console.WriteLine("Год Кролика"); break;
+            //        Console.WriteLine("Год Кролика"); break;
+            //    case  2024 :Console.WriteLine("Год Дракона"); break;
+            //    case 2012: Console.WriteLine("Год Дракона"); break;
+            //    case 2000: Console.WriteLine("Год Дракона"); break;
+            //    case 1988: Console.WriteLine("Год Дракона"); break;
+
+            //    case 1976: Console.WriteLine("Год Дракона"); break;
+            //    case 1964: Console.WriteLine("Год Дракона"); break;
+            //    case 1952: Console.WriteLine("Год Дракона"); break;
+            //    case 1940: Console.WriteLine("Год Дракона"); break;
+            //        Console.WriteLine("Год Дракона"); break;
+            //    case 2025: case 2013: case 2001: case 1989: case 1977: case 1965: case 1953:case 1941: Console.WriteLine("Год Змеи "); break;
+            //    case 2026: case 2014: case 2002: case 1990: case 1978: case 1966: case 1954: case 1942: Console.WriteLine("Год Лошади "); break;
+            //    case 2027: case 2015: case 2003: case 1991: case 1979: case 1967: case 1955: case 1943: Console.WriteLine("Год Козы "); break;
+            //    case 2028: case 2016: case 2004: case 1992: case 1980: case 1968: case 1956: case 1944: Console.WriteLine("Год Обезьяны "); break;
+            //    case 2029: case 2017: case 2005: case 1993: case 1981: case 1969: case 1957: case 1945: Console.WriteLine("Год Петуха  "); break;
+            //    case 2030: case 2018: case 2006: case 1994: case 1982: case 1970: case 1958: case 1946: Console.WriteLine("Год Собаки "); break;
+            //    case 2031: case 2019: case 2007: case 1995: case 1983: case 1971: case 1959: case 1947: Console.WriteLine("Год Свиньи "); break;
+            //}
+            //Console.ReadKey();
+           
+        }
+    }
+}
